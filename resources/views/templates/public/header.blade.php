a7103252a21a21b8a7bf21004af4abaeb43bbe95
narwinoks <narnowin195@gmail.com>
2024-06-06 11:23:12 +0700

    feat(tag) : add google tag

--- resources/views/templates/public/header.blade.php
+++ resources/views/templates/public/header.blade.php
@@ -9,7 +9,15 @@
     <meta name="description" content="Jasamedika Transmedic" />
     <meta name="author" content="https://jasamedikatransmedic.com/" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZET9LH4LQ7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'G-ZET9LH4LQ7');
+    </script>
     @include('templates.public.styles')
     @stack('styles')
 </head>
